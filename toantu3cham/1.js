98c3f8ec183a05ed1095b99fa7f3cb59a9c4efb2
kienz7777 <kienvu729>
2020-02-21 19:14:17 +0700

    first-test

---
+++ toantu3cham/1.js
@@ -0,0 +1,25 @@
+// var p1 = {
+//     ten : "tivi",
+//     gia : 90
+// }
+
+// console.log(p1);
+
+// var p2 = {...p1};
+
+// p2.gia = 100;
+
+// console.log(p2);
+
+// console.log(p1);
+
+var a1 = [1,2,3];
+
+console.log(a1);
+
+var a2 = [...a1];
+
+a2[0] = 100;
+
+console.log(a2);
+console.log(a1);
